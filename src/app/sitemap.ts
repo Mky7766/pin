7f4571745367f40546ccc490459c4d1f1023a446
Mukesh Yogi <mukeshyogi2802@gmail.com>
2025-10-16 04:49:37 +0000

    site map ko aesa luck do (This XML file does not appear to have any styl

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@
 import { MetadataRoute } from 'next';
 
 // This function can be updated to fetch dynamic routes from a database.
-async function getDynamicRoutes() {
+async function getDynamicRoutes(): Promise<MetadataRoute.Sitemap> {
   const posts = [
     {
       slug: 'how-to-drop-a-pin',
@@ -32,51 +32,37 @@ async function getDynamicRoutes() {
 
   return posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: new Date(post.updatedAt),
     changeFrequency: 'yearly',
-    priority: 0.8,
   }));
 }
 
 const baseUrl = 'https://droppedpin.xyz';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const staticRoutes = [
+  const staticRoutes: MetadataRoute.Sitemap = [
     {
       url: `${baseUrl}/`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
+      changeFrequency: 'daily',
     },
     {
       url: `${baseUrl}/blog`,
-      lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 0.9,
     },
     {
       url: `${baseUrl}/about`,
-      lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
     },
     {
       url: `${baseUrl}/disclaimer`,
-      lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.7,
     },
     {
       url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.7,
     },
     {
       url: `${baseUrl}/terms-and-conditions`,
-      lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.7,
     },
   ];
 
